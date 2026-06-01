037d2c4d2b03a3424fa14253f38820007933beba
tiagoworking5 <tiagoworking5@gmail.com>
2026-05-31 21:15:51 -0300

    Create exercicio6.py
    exercício 6 página 51 do ebook python.

---
+++ exercicio6.py
@@ -0,0 +1,7 @@
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+delta = b ** 2 - 4 * a * c
+
+print(f"Delta = {delta:.2f}")
